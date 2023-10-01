283173c1aca7cf422af34c21e370d33aa1a96fe1
Pedro H <pedro_gonsalves@hotmail.com>
2023-10-01 15:41:03 -0300

    add: connection class with database

---
+++ database/connection.py
@@ -0,0 +1,21 @@
+from configs import get_var
+from sqlalchemy import create_engine, text
+
+
+class DatabaseConnection:
+    def __init__(self):
+        self.host = get_var('DB_HOST')
+        self.port = get_var('DB_PORT')
+        self.dbName = get_var('DB_NAME')
+        self.user = get_var('DB_USER')
+        self.password = get_var('DB_PASSWORD')
+        self.__engine__ = create_engine(
+            f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbName}"
+        )
+
+    def execute(self, raw_statement: str):
+        with self.__engine__.connect() as connection:
+            statement = text(raw_statement)
+            cursorStatement = connection.execute(statement)
+            resultOfQuery = cursorStatement.fetchall()
+        return resultOfQuery
