3ae048b5c9028c3870f98afe52053dfe3c9a749a
Pedro H <pedro_gonsalves@hotmail.com>
2023-10-05 23:32:39 -0300

    add: games round query and refac games played query

--- database/raw_statements.py
+++ database/raw_statements.py
@@ -1,7 +1,20 @@
-def games_played_round():
-    return """
-    select cast(replace("round",'Regular Season -', '') as integer) "round", count("round") from ftd.fixtures
+def games_played_round(id_league: int, season: int):
+    return f"""
+    select cast(replace("round",'Regular Season -', '') as integer) "round", count("round") from ftd.fixtures fx
+    inner join ftd.leagues l on l.id = fx.id_league
     where status = 'match_finished'
+    and l.id_league = {id_league}
+    and l.season = {season}
     group by cast(replace("round",'Regular Season -', '') as integer)
     order by cast(replace("round",'Regular Season -', '') as integer)
+    """
+
+def games_round(round:int, id_league:int, season:int):
+    return f"""
+    select fx.id from ftd.fixtures fx 
+    inner join ftd.leagues l on l.id = fx.id_league 
+    where status != 'match_finished'
+    and l.id_league = {id_league}
+    and l.season = {season}
+    and cast(replace("round",'Regular Season -', '') as integer) = {round}
     """
