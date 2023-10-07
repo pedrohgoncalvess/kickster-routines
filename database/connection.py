df6e7d5736ec23c5a9e669567d0d50edbb39e948
Pedro H <pedro_gonsalves@hotmail.com>
2023-10-06 23:13:46 -0300

    add: some methods will return results of query, problably i will remove this methods and create a new class before

--- database/connection.py
+++ database/connection.py
@@ -13,7 +13,7 @@ class DatabaseConnection:
         self.__engine__ = create_engine(
             f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbName}"
         )
-        self.session = Session(self.__engine__)
+
 
     def execute(self, raw_statement: str):
         with self.__engine__.connect() as connection:
@@ -21,3 +21,54 @@ class DatabaseConnection:
             cursorStatement = connection.execute(statement)
             resultOfQuery = cursorStatement.fetchall()
         return resultOfQuery
+
+    def local_session(self):
+        return Session(self.__engine__)
+
+    def execute_orm(self, objectToSave):
+        session = self.local_session()
+        try:
+            session.add(objectToSave)
+            session.commit()
+        except Exception as error:
+            print(error)
+        finally:
+            session.close()
+
+
+    def leagues_metadata(self, id_league: int):
+        from database.raw_statements import leagues_metadata
+
+        query = leagues_metadata(id_league)
+        results = self.execute(query)
+        listOfLeaguesMetadata = []
+        for result in results:
+            listOfLeaguesMetadata.append(
+                {"id_league":result[0],"id_metadata":result[1],"total_rounds":result[3],"games_round":result[4]}
+            )
+        return listOfLeaguesMetadata
+
+    def incomplet_rounds_metadata(self, id_league_metadata: int):
+        from database.raw_statements import incompleted_rounds_metadata
+
+        query = incompleted_rounds_metadata(id_league_metadata)
+        results = self.execute(query)
+        listOfRoundsIncompleted = []
+        for result in results:
+            listOfRoundsIncompleted.append(
+                {"id_league_mtd":result[1],"round":result[2]}
+            )
+        return listOfRoundsIncompleted
+
+    def fixtures_played_round(self, id_league:int, season:int):
+        from database.raw_statements import games_played_round
+
+        query = games_played_round(id_league, season)
+        results = self.execute(query)
+
+        listOfRounds = []
+        for result in results:
+            listOfRounds.append(
+                {"round":result[0], "games_played":result[1]}
+            )
+        return listOfRounds
