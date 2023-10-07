97be20e58cb89cb9a03fe29c3c612f83696477d3
Pedro H <pedro_gonsalves@hotmail.com>
2023-10-07 02:48:26 -0300

    refac: fixtures_to_collect function, removing fors and call methods

--- routines/fixtures/routine.py
+++ routines/fixtures/routine.py
@@ -1,38 +1,31 @@
 from api_sports.api_requests import APIRequest
-from database.raw_statements import games_played_round
 from routines.fixtures.parser import fixture_parser
-from routines.fixtures.metadata import check_rounds_brasileirao, insert_metadata
+from sqlalchemy import select
 from configs import actual_season
 
 
-def att_rounds_brasileirao():
+def fixtures_to_collect(id_league: int = 71):
     from database.connection import DatabaseConnection
 
     dbConnection = DatabaseConnection()
-
-    leagueMetadata = dbConnection.leagues_metadata(71)[0]
-
-    idLeague = leagueMetadata.get("id_league")
+    leaguesMetadata = dbConnection.leagues_metadata(id_league)
+    idMetadata = leaguesMetadata.get("id_metadata")
+    idLeague = leaguesMetadata.get("id_league")
     seasonLeague = actual_season()
-    gamesRoundLeague = leagueMetadata.get("games_round")
-    idMetadata = leagueMetadata.get("id_metadata")
 
-    roundsPlayed = dbConnection.execute(games_played_round(idLeague, seasonLeague))
+    fixturesId = []
+    roundsInDatabase = dbConnection.incomplet_rounds_metadata(idMetadata)
+    incompletRounds = roundsInDatabase.get("rounds")
+    for roundToCollect in incompletRounds:
+        _round = roundToCollect.get("round")
+        fixturesToCollectOfRound = dbConnection.fixtures_to_collect(_round,idLeague,seasonLeague)
+        for fixtureIdOnRound in fixturesToCollectOfRound:
+            fixturesId.append(fixtureIdOnRound)
 
-    idLeagueMtd = 2 #metadata brasileirao
-    roundsIncomplet = dbConnection.incomplet_rounds_metadata(idLeagueMtd)
-    roundsPlayed = dbConnection.leagues_metadata()
+    return fixturesId
 
-
-
-def main():
-
-    roundsChecked = check_rounds_brasileirao()
-
-    insert_metadata(roundsChecked)
 
 def collect_fixture(id_fixture: int):
-
     requestClass = APIRequest()
     fixtureBasicsInfos = requestClass.fixture_basics(id_fixture)
     treatedInfos = fixture_parser(fixtureBasicsInfos[0])
@@ -40,7 +33,6 @@ def collect_fixture(id_fixture: int):
 
 
 def update_fixture(fixture_infos):
-    from sqlalchemy import select
     from database.models.ftd.ftd_fixtures_model import Fixtures
     from database.connection import DatabaseConnection
 
@@ -57,7 +49,3 @@ def update_fixture(fixture_infos):
     fixtureDb.start_at = fixture_infos.get("date")
     session.commit()
     session.close()
-
-
-#update_fixture(collect_fixture(1005893))
-main()
